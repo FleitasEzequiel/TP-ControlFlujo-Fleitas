894e7d400a2d0855f36c90f50d52eb72cc894906
Ezequiel <ezequielafeita@gmail.com>
2024-05-14 10:45:55 -0300

    Se agregó el script parImpar.js

---
+++ assets/js/parImpar.js
@@ -0,0 +1,14 @@
+let numero = 0;
+while (numero <= 10) {
+switch (numero % 2) {
+    case 0:
+        console.log( numero + ": Número Par")
+        break;
+    case 1:
+        console.log( numero + ": Número Impar")
+        break;
+        default:
+                        console.log("Número Inválido")
+        break;
+}
+numero ++}
